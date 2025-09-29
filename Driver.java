884d1d2c4a67e7bc7fb962cbf55cada77bad9ab4
abcdJus <mkmin5577@gmail.com>
2025-09-28 23:33:28 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,17 +1,38 @@
+import java.io.File;
+
 public class Driver {
 	public static void main(String [] args) {
-		Polynomial p = new Polynomial();
-		System.out.println(p.evaluate(3));
-		double [] c1 = {6,0,0,5};
-		Polynomial p1 = new Polynomial(c1);
-		double [] c2 = {0,-2,0,0,-9};
-		Polynomial p2 = new Polynomial(c2);
+		double[] cz = {};
+		int[] ez = {};
+		Polynomial p = new Polynomial(cz, ez);
+		System.out.println("p(3) = " + p.evaluate(3));
+
+		double [] c1 = {6, 0, 0, 5};
+		int [] e1 = {0, 1, 2, 3};
+		Polynomial p1 = new Polynomial(c1, e1);
+
+		double [] c2 = {0, -2, 0, 0, -9};
+		int [] e2 = {0, 1, 2, 3, 4};
+		Polynomial p2 = new Polynomial(c2, e2);
+
 		Polynomial s = p1.add(p2);
 		System.out.println("s(0.1) = " + s.evaluate(0.1));
-
-		if(s.hasRoot(1))
+		if (s.hasRoot(1))
 			System.out.println("1 is a root of s");
 		else
 			System.out.println("1 is not a root of s");
+
+		Polynomial m = p1.multiply(p2);
+		System.out.println("m(1) = " + m.evaluate(1));
+		System.out.println("m(2) = " + m.evaluate(2));
+
+		String fname = "poly.txt";
+		s.saveToFile(fname);
+		try {
+			Polynomial fromFile = new Polynomial(new File(fname));
+			System.out.println("fromFile(0.1) = " + fromFile.evaluate(0.1));
+		} catch (Exception ex) {
+			System.out.println("File load failed: " + ex.getMessage());
+		}
 	}		
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,46 +1,286 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+
 public class Polynomial {
     private double[] coef;
+    private int[] exp;
 
-    Polynomial() {  
-        coef = new double[]{0};
+    Polynomial() {
     }
 
-    Polynomial(double[] coef) {
+    Polynomial(double[] coef, int[] exp) {
         this.coef = coef;
+        this.exp = exp;
+    }
+
+    Polynomial(File file) {
+        String line;
+
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            line = br.readLine();
+        } catch (Exception num2) {
+            this.coef = new double[0];
+            this.exp = new int[0];
+            return;
+        }
+
+        if (line == null || line.trim().length() == 0) {
+            this.coef = new double[0];
+            this.exp = new int[0];
+            return;
+        }
+
+        String str = line.trim();
+
+        StringBuilder normalized = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            char num = str.charAt(i);
+            if (i > 0 && num == '-') normalized.append("+-");
+            else normalized.append(num);
+        }
+
+        String[] terms = normalized.toString().split("\\+");
+
+        double[] tmpCoef = new double[terms.length];
+        int[] tmpExp = new int[terms.length];
+        int size = 0;
+
+        for (String term : terms) {
+            if (term == null || term.trim().isEmpty()) continue;
+            term = term.trim();
+
+            double coeffValue;
+            int exponentValue;
+
+            int xIndex = term.indexOf('x');
+            if (xIndex >= 0) {
+                String coeffStr = term.substring(0, xIndex);
+                String expStr = term.substring(xIndex + 1);
+
+                if (coeffStr.length() == 0 || "+".equals(coeffStr)) coeffValue = 1.0;
+                else if ("-".equals(coeffStr)) coeffValue = -1.0;
+                else coeffValue = Double.parseDouble(coeffStr);
+
+                exponentValue = (expStr.length() == 0) ? 1 : Integer.parseInt(expStr);
+            } else {
+                coeffValue = Double.parseDouble(term);
+                exponentValue = 0;
+            }
+
+            boolean found = false;
+            for (int i = 0; i < size; i++) {
+                if (tmpExp[i] == exponentValue) {
+                    tmpCoef[i] += coeffValue;
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found) {
+                tmpCoef[size] = coeffValue;
+                tmpExp[size] = exponentValue;
+                size++;
+            }
+        }
+
+        int count = 0;
+        for (int i = 0; i < size; i++) {
+            if (tmpCoef[i] != 0.0) count++;
+        }
+
+        if (count == 0) {
+            this.coef = new double[0];
+            this.exp = new int[0];
+            return;
+        }
+
+        this.coef = new double[count];
+        this.exp = new int[count];
+        int k = 0;
+        for (int i = 0; i < size; i++) {
+            if (tmpCoef[i] != 0.0) {
+                this.coef[k] = tmpCoef[i];
+                this.exp[k] = tmpExp[i];
+                k++;
+            }
+        }
+    }   
+
+    void saveToFile(String filename) {
+        StringBuilder sb = new StringBuilder();
+
+        if (this.coef == null || this.coef.length == 0) {
+            sb.append("0");
+        } else {
+            boolean yes = true;
+            for (int i = 0; i < this.coef.length; i++) {
+                double num = this.coef[i];
+                int num2 = this.exp[i];
+                
+                if (Math.abs(num) == 0) continue;
+
+                if (yes) {
+                    if (num < 0) sb.append("-");
+                } else {
+                    if (num < 0) sb.append("-");
+                    else sb.append("+");
+                }
+
+                double absCoef = Math.abs(num);
+                
+                if (num2 == 0) {
+                    if (absCoef == (long)absCoef) {
+                        sb.append((long)absCoef);
+                    } else {
+                        sb.append(absCoef);
+                    }
+                } else {
+                    if (Math.abs(absCoef - 1.0) != 0) {
+                        if (absCoef == (long)absCoef) {
+                            sb.append((long)absCoef);
+                        } else {
+                            sb.append(absCoef);
+                        }
+                    }
+                    
+                    sb.append("x");
+                    if (num2 != 1) {
+                        sb.append(num2);
+                    }
+                }
+
+                yes = false;
+            }
+
+            if (sb.length() == 0) sb.append("0");
+        }
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(filename))) {
+            bw.write(sb.toString());
+        } catch (Exception num2) {
+        }
     }
 
     Polynomial add(Polynomial p) {
-        int len = this.coef.length;
+        if (p == null) return new Polynomial(this.coef, this.exp);
 
-        if (p.coef.length > len) 
-            len = p.coef.length;
-    
+        int maxLength = this.coef.length + p.coef.length;
+        double[] tempCoefs = new double[maxLength];
+        int[] tempexp = new int[maxLength];
+        int currentSize = 0; 
 
-        double[] result = new double[len];
-
-        for (int i = 0; i < len; i++) {
-            double num1 = 0;
-            double num2 = 0;
-
-            if (i < this.coef.length) 
-                num1 = this.coef[i];
-            
-            if (i < p.coef.length) 
-                num2 = p.coef[i];
-            
-
-            result[i] = num1 + num2;
+        for (int i = 0; i < this.coef.length; i++) {
+            tempCoefs[currentSize] = this.coef[i];
+            tempexp[currentSize] = this.exp[i];
+            currentSize++;
         }
 
-        return new Polynomial(result);
+        for (int i = 0; i < p.coef.length; i++) {
+            int pExp = p.exp[i];
+            double pCoef = p.coef[i];
+            boolean foundMatch = false;
+
+            for (int j = 0; j < currentSize; j++) {
+                if (tempexp[j] == pExp) {
+                    tempCoefs[j] += pCoef;
+                    foundMatch = true;
+                    break;
+                }
+            }
+
+            if (!foundMatch) {
+                tempCoefs[currentSize] = pCoef;
+                tempexp[currentSize] = pExp;
+                currentSize++;
+            }
+        }
+
+        int finalSize = 0;
+        for (int i = 0; i < currentSize; i++) {
+            if (Math.abs(tempCoefs[i]) != 0) finalSize++;
+        }
+
+        if (finalSize == 0) {
+            return new Polynomial();
+        }
+
+        double[] resultcoef = new double[finalSize];
+        int[] resultexp = new int[finalSize];
+        int destIndex = 0;
+        for (int i = 0; i < currentSize; i++) {
+            if (Math.abs(tempCoefs[i]) != 0) {
+                resultcoef[destIndex] = tempCoefs[i];
+                resultexp[destIndex] = tempexp[i];
+                destIndex++;
+            }
+        }
+
+        return new Polynomial(resultcoef, resultexp);
+    }
+
+    public Polynomial multiply(Polynomial p) {
+        if (p == null) return new Polynomial();
+        
+        if (this.coef.length == 0 || (this.coef.length == 1 && this.coef[0] == 0) || p.coef.length == 0 || (p.coef.length == 1 && p.coef[0] == 0)) return new Polynomial();
+
+        int maxProductTerms = this.coef.length * p.coef.length;
+        double[] tempCoefs = new double[maxProductTerms];
+        int[] tempexp = new int[maxProductTerms];
+        int currentSize = 0;
+
+        for (int i = 0; i < this.coef.length; i++) {
+            for (int j = 0; j < p.coef.length; j++) {
+                double productCoef = this.coef[i] * p.coef[j];
+                int productExp = this.exp[i] + p.exp[j];
+
+                if (Math.abs(productCoef) == 0) continue;
+
+                boolean foundMatch = false;
+                for (int k = 0; k < currentSize; k++) {
+                    if (tempexp[k] == productExp) {
+                        tempCoefs[k] += productCoef;
+                        foundMatch = true;
+                        break;
+                    }
+                }
+
+                if (!foundMatch) {
+                    tempCoefs[currentSize] = productCoef;
+                    tempexp[currentSize] = productExp;
+                    currentSize++;
+                }
+            }
+        }
+
+        int finalSize = 0;
+        for (int i = 0; i < currentSize; i++) {
+            if (Math.abs(tempCoefs[i]) > 0) finalSize++;
+        }
+
+        if (finalSize == 0) return new Polynomial();
+
+        double[] resultcoef = new double[finalSize];
+        int[] resultexp = new int[finalSize];
+        int destIndex = 0;
+        for (int i = 0; i < currentSize; i++) {
+            if (Math.abs(tempCoefs[i]) > 0) {
+                resultcoef[destIndex] = tempCoefs[i];
+                resultexp[destIndex] = tempexp[i];
+                destIndex++;
+            }
+        }
+
+        return new Polynomial(resultcoef, resultexp);
     }
 
     public double evaluate(double x) {
         double sum = 0;
-
-        for (int i = 0; i < coef.length; i++) 
-            sum += coef[i] * Math.pow(x, i);
-        
+        for (int i = 0; i < coef.length; i++) {
+            sum += coef[i] * Math.pow(x, exp[i]);
+        }
         return sum;
     }
 
@@ -48,3 +288,5 @@ public class Polynomial {
         return evaluate(x) == 0;
     }
 }
+
+
